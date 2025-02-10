aba40a004d628bb5cb91b99e8d477aa3f0c3ba10
eminentetim <etimeminent@gmail.com>
2025-02-10 12:49:41 +0100

    database setup

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from listings import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel App API",
+        default_version='v1',
+        description="API documentation for Travel App",
+        contact=openapi.Contact(email="contact@travelapp.local"),
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),
+    path('', views.index, name='home'), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
